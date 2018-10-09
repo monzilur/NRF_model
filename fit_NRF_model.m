7df4fab9e6c951bdc46d6f69e3cfd4b9c1c9ec18
Monzilur Rahman <monzilur.rahman@gmail.com>
2018-10-09 14:45:57 +0100

    Update fit_NRF_model.m

--- fit_NRF_model.m
+++ fit_NRF_model.m
@@ -1,4 +1,4 @@
-function [theta,train_err,lam] = fit_NRF_model(X_fht,y_t,minibatch_number,...
+function [theta,train_err] = fit_NRF_model(X_fht,y_t,minibatch_number,...
     regtype,lam,net_str,num_pass,theta_init)
 % [theta,train_err] = fit_NRF_model(X_fht,y_t,minibatch_number,...
 %    regtype,lam,net_str,num_pass,theta_init)
@@ -62,27 +62,26 @@ end
 I = size(X_fht,1)*size(X_fht,2);
 
 % Initialization of network parameters
-for ii=1:length(lam)
-    if ~exist('theta_init','var')
-        rng('shuffle');
-        C = 0.5;
-        W_jk = C*2*(rand(K,J)-0.5)/sqrt(J+K);
-        W_ij = C*2*(rand(J,I)-0.5)/sqrt(I+J);
-        b_k = C*2*(rand(K,1)-0.5)/sqrt(J+K);
-        b_j = C*2*(rand(J,1)-0.5)/sqrt(I+J);
+if ~exist('theta_init','var')
+    rng('shuffle');
+    C = 0.5;
+    W_jk = C*2*(rand(K,J)-0.5)/sqrt(J+K);
+    W_ij = C*2*(rand(J,I)-0.5)/sqrt(I+J);
+    b_k = C*2*(rand(K,1)-0.5)/sqrt(J+K);
+    b_j = C*2*(rand(J,1)-0.5)/sqrt(I+J);
 
-        theta_init = {W_jk,W_ij,b_k,b_j};
-    end
-
-    args = {J,K,lam(ii),regtype};
-    % initialize the optimizer
-    optimizer = sfo(@loss_function_NRF,theta_init,sub_refs,args);
-    % run the optimizer for half a pass through the data
-    theta{ii,:} = optimizer.optimize(0.5);
-    % run the optimizer for another 20 passes through the data, continuing from 
-    % the theta value where the prior call to optimize() ended
-    theta{ii,:} = optimizer.optimize(num_pass);
-
-    train_err(ii,:) = optimizer.hist_f_flat;
+    theta_init = {W_jk,W_ij,b_k,b_j};
 end
+
+% Training
+args = {J,K,lam,regtype};
+% initialize the optimizer
+optimizer = sfo(@loss_function_NRF,theta_init,sub_refs,args);
+% run the optimizer for half a pass through the data
+theta = optimizer.optimize(0.5);
+% run the optimizer for another 20 passes through the data, continuing from 
+% the theta value where the prior call to optimize() ended
+theta = optimizer.optimize(num_pass);
+
+train_err = optimizer.hist_f_flat;
 end
